e872355f57df5c86c61af3f09980c00b4fb7c893
Alexander Putilin <avp-13@yandex.ru>
2025-01-08 03:56:33 +0000

    Fix build

--- apps/web/app/(app)/questions/[marketId]/[slug]/irl/page.tsx
+++ apps/web/app/(app)/questions/[marketId]/[slug]/irl/page.tsx
@@ -1,9 +1,11 @@
 import { revalidateTag } from 'next/cache'
 import { getExtendedMarket } from '@play-money/api-helpers/client'
 import { MarketIrlPage } from '@play-money/markets/components/MarketIrlPage'
+import { ExtendedMarket } from '@play-money/markets/types'
 
 export default async function AppPostsSlugPage({ params }: { params: { marketId: string } }) {
-  const market = await getExtendedMarket({ marketId: params.marketId })
+  const marketRes = await getExtendedMarket({ marketId: params.marketId })
+  const market: ExtendedMarket = marketRes.data
 
   // eslint-disable-next-line @typescript-eslint/require-await -- Next requires this to be async since its SSR
   const handleRevalidate = async () => {
@@ -11,5 +13,5 @@ export default async function AppPostsSlugPage({ params }: { params: { marketId:
     revalidateTag(`market:${params.marketId}`)
   }
 
-  return <MarketIrlPage market={market.data} onRevalidate={handleRevalidate} renderActivitiy={null} />
+  return <MarketIrlPage market={market} onRevalidate={handleRevalidate} renderActivitiy={null} />
 }
